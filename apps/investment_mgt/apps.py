19b5997504c98387b6b33e53625cd0575cc03f7a
nikeisme <nikeisme827@gmail.com>
2022-09-19 19:16:26 +0900

    :tada: Feat: 프로젝트 생성

---
+++ apps/investment_mgt/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class InvestManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.investment_mgt'
---
+++ backend_core/urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/invest-management/', include('apps.investment_mgt.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    import debug_toolbar
+
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls))
+    ]
